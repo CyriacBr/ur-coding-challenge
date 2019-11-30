c7d82d6bce9f4b185e8e405a8ef210cb991a9394
CyriacBr <cyriacbr@gmail.com>
2019-11-30 20:09:14 +0100

    Revert "feat: working on auth service"
    This reverts commit 6e361a0342d8c6f7a13b1c564b908f0f0f1c16f1.

--- packages/common/src/auth.ts
+++ packages/common/src/auth.ts
@@ -1,16 +1,7 @@
-import { IUserProfile } from "./userProfile";
 
 export namespace AuthDTO {
   export interface signUp {
     email: string;
     password: string;
-  }
-  export interface signIn {
-    email: string;
-    password: string;
-  }
-  export interface payload {
-    userId: number;
-    profile: IUserProfile;
   }
 }
