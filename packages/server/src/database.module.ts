ff2574fbe84ada8d88f44b99538bb053a1c697f8
CyriacBr <cyriacbr@gmail.com>
2019-11-30 21:05:40 +0100

    feat: added database module

---
+++ packages/server/src/database.module.ts
@@ -0,0 +1,18 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'root',
+      database: 'ur_code_challenge_cyriac',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true,
+    }),
+  ],
+})
+export class DatabaseModule {}
